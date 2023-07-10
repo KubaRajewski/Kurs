7c95060657811ea26a5b2ff451329b085361e95b
KubaRajewski <66092469+KubaRajewski@users.noreply.github.com>
2023-07-10 13:58:41 +0200

    commit

--- src/iteracjaPierwsza/tablice/zadaniaPDF/Main.java
+++ src/iteracjaPierwsza/tablice/zadaniaPDF/Main.java
@@ -17,22 +17,97 @@ public class Main {
 
 //                                      {4, 5, 6} → {0, 0, 0, 0, 0, 6}
 
-        int[] table2 = {4, 5, 6};
-        int last = table2[table2.length - 1];
-        System.out.println(last);
+        int[] t = {4, 5, 6};
+
+        int last = t[t.length - 1];
+        int length = (t.length) * 2;
+        int[] t2 = new int[length];
+        t2[t2.length - 1] = last;
+        for (int i = 0; i < t2.length; i++) {
+            System.out.print(t2[i] + " ");
+        }
+        System.out.println();
 
 // Napisz program, który dla danej tablicy intów zwraca sume elementów po lewej od zadanego indeksu. Np. dla danych:
 // {1,2,3,4,5} i indeksu = 2 suma wynosi 3 bo 1 + 2 = 3
 
+        int[] t3 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int indx = 2;
+        int sum3 = 0;
+
+        for (int i = 0; i < indx; i++) {
+            sum3 += t3[i];
+        }
+        System.out.println(sum3);
 
 // Zadeklaruj tablice stringów dlugosci 4, dodaj do niej 4 wyrazy.
 // W pętli przypisz każdy wyraz do wcześniej utworzonej zmiennej typu String. Wyświetl tą zmienną.
 
+        String[] words = {"pies", "kot", "wiewiorka", "zegarek"};
+        String result = "";
+
+
+        for (int i = 0; i < words.length; i++) {
+            result += words[i] + " ";
+        }
+
+        System.out.println(result);
+
+
 // Zadeklaruj dwie tablice intów róznej długości. Przypisz obu na indeksie 0 jakąś liczbę.
 // Wyświetl tę tablicę, która jest dłuzsza (oczywiscie zmiana długosci danej tablicy powinna wpływać na wypisanie tablic).
 
+        int[] table1 = new int[5];
+        int[] table2 = new int[10];
 
-// Dla podanej tablicy intow oblicz czy więcej jest elementów dodatnych czy podzielnych przez 3
+        table1[0] = 12;
+        table2[0] = 5;
+
+        if (table1.length > table2.length) {
+            drukujTablice(table1);
+        } else if (table1.length == table2.length) {
+            System.out.println("tablice sa tej samej dlugosci");
+        } else {
+            drukujTablice(table2);
+        }
+    }
+
+    public static void drukujTablice(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        System.out.println();
+
+// Dla podanej tablicy intow oblicz czy więcej jest elementów dodatnich czy podzielnych przez 3
+
+        int[] t4 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        int podzielne = 0;
+        int dodatnie = 0;
+
+        for (int i = 0; i < t4.length; i++) {
+            if (t4[i] > 0) {
+                dodatnie = dodatnie + 1;
+            }
+        }
+
+        for (int i = 0; i < t4.length; i++) {
+            if (t4[i] % 3 == 0) {
+                podzielne = podzielne + 1;
+            }
+        }
+
+        if (dodatnie > podzielne) {
+            System.out.println("liczb dodatnich jest wiecej niz liczb podzielnych przez 3, " +
+                    "Liczby dodatnie: " + dodatnie + ", liczby podzielne przez 3: " + podzielne);
+        } else if (dodatnie == podzielne) {
+            System.out.println("liczb dodatnich jest tyle samo co podzielnych przez 3: " + dodatnie);
+        } else {
+            System.out.println("liczb podzielnych przez 3 jest wiecej niz liczb dodatnich, liczby podzielne przez 3: " + podzielne
+                    + "liczby dodatnie: " + dodatnie);
+        }
 
 // Z podanej tablicy Stringów wypisz te których długość jest większ niż 7 (długość Stringa zwróci nam metoda length(),
 // czyli np dla Stringa imie = ”Ania”; imie.length() zwraca 4)
@@ -43,5 +118,7 @@ public class Main {
 
 // Dla podanej tablicy Stringów, wypisz te, które kończą się na literę ‘a’, (metoda charAt() lub endsWith(), wygoogluj
 // co one robią i jak działają)
+
     }
+
 }
