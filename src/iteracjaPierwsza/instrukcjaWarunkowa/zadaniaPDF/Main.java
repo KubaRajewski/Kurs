4ff5498bcb2a70fc137e528262f80da6e9198be5
KubaRajewski <66092469+KubaRajewski@users.noreply.github.com>
2023-07-10 11:55:02 +0200

    commit

--- src/iteracjaPierwsza/instrukcjaWarunkowa/zadaniaPDF/Main.java
+++ src/iteracjaPierwsza/instrukcjaWarunkowa/zadaniaPDF/Main.java
@@ -4,18 +4,73 @@ public class Main {
     public static void main(String[] args) {
 
 // 1) Zadeklaruj dwie zmienne typu String i spróbuj je porównać operatorem >. Jaki jest efekt programu?
+/*
+        String imie = "Kuba";
+        String nazwisko = "Rajewski";
 
+        if (imie > nazwisko) {
+            System.out.println("imie jest dluzsze");
+        }
 
+        Program nie zadziala, jesli chcielibysmy porownac dlugosc stringow trzeba by uzyc metody length
+
+*/
 // 2) Napisz program w którym deklarujesz 3 zmienne typu int (różne od siebie) i program wyświetli najmniejszą z nich.
 
+        int a = 9;
+        int b = 3;
+        int c = 6;
+
+        int najmniejsza;
+
+        if (a <= b && a <= c) {
+            najmniejsza = a;
+        } else if (b <= a && b <= c) {
+            najmniejsza = b;
+        } else {
+            najmniejsza = c;
+        }
+
+        System.out.println("Najmniejsza zmienna to: " + najmniejsza);
 
 // 3) Napisz program w którym deklarujesz 2 zmienne typu int. Sprawdź czy są sobie równe
 
+        int d = 3;
+        int e = 3;
 
+        if (d == e) {
+            System.out.println("sa rowne");
+        }
 // 4) Napisz program który sprawdzi czy średnia dwóch liczb typu double jest większa od 5.
 
+        double x = 5;
+        double y = 15;
+
+        if (((x + y) / 2) > 5) {
+            System.out.println("średnia jest wieksza niz 5");
+        }
 
 // 5*) Napisz program w którym deklarujesz 3 zmienne typu int (różne od siebie). Program ma wyświetlić je posortowane rosnąco
 
+        if (a <= b && a <= c) {
+            if (b <= c) {
+                System.out.println(a + ", " + b + ", " + c);
+            } else {
+                System.out.println(a + ", " + c + ", " + b);
+            }
+        } else if (b <= a && b <= c) {
+            if (a <= c) {
+                System.out.println(b + ", " + a + ", " + c);
+            } else {
+                System.out.println(b + ", " + c + ", " + a);
+            }
+        } else {
+            if (a <= b) {
+                System.out.println(c + ", " + a + ", " + b);
+            } else {
+                System.out.println(c + ", " + b + ", " + a);
+            }
+        }
     }
 }
+
--- src/iteracjaPierwsza/petle/zadaniaPDF/Main.java
+++ src/iteracjaPierwsza/petle/zadaniaPDF/Main.java
@@ -1,39 +1,105 @@
 package iteracjaPierwsza.petle.zadaniaPDF;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-// Napisz program, który oblicza potęgę 2n, gdzie liczbę naturalną n podaje użytkownik. Dla n = 10, wynik to 210 = 1024
+// Napisz program, który oblicza potęgę 2n, gdzie liczbę naturalną n podaje użytkownik. Dla n = 10, wynik to 2^10 = 1024
+        Scanner scanner = new Scanner(System.in);
+/*
 
+        System.out.print("n: ");
+        int n = scanner.nextInt();
 
+        int wynik = 1;
 
+        for (int i = 0; i < n; i++) {
+            wynik *= 2;
+        }
+
+        System.out.println("Wynik to 2^" + n + " = " + wynik);
+
+*/
 // Napisz program drukujacy na ekranie prostokat z literek X. Wysokosc i szerokosc prostokata wczytujemy z klawiatury.
 // Dla szerokosci = 10 oraz wysokosci = 4 tak powinien wygladac prostokat:
 //
-//        XXXXXXXXXX
-//        X        X
-//        X        X
-//        XXXXXXXXXX
+//                                          XXXXXXXXXX
+//                                          X        X
+//                                          X        X
+//                                          XXXXXXXXXX
+/*
+        System.out.print("Height: ");
+        int height = scanner.nextInt();
+        System.out.print("Width: ");
+        int width = scanner.nextInt();
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
+                if (i == 1 || i == height || j == 1 || j == width) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
 
-
+/*
 // Napisz program, który oblicza średnią arytmetyczną liczb naturalnych od 5 do liczby podanej przez użytkownika.
 // Wypisać obliczoną średnią na konsoli.
 // Dla n = 9 program ma wypisać (5+6+7+8+9)/5 = 35/5= 7
+/*
+        System.out.print("n: ");
+        int n = scanner.nextInt();
 
+        // chcielibysmy miec niezdefiniowana tablice, zrobic petle od 5 do n i za kazda iteracja dodawac ta liczbe do
+        // tablicy w ktorej pierwszym wyrazem bedzie 5, potem policzyc srednia czyli dodac do siebie wszystkie elementy
+        // i podzielic przez ilosc elementow w tablicy
 
+        // Tworzenie pustej tablicy z dynamicznym rozmiarem
+        ArrayList<Integer> array = new ArrayList<>();
+
+        // Dodawanie liczb od 5 do n do tablicy
+        for (int i = 5; i <= n; i++) {
+            array.add(i);
+        }
+
+        // Obliczanie sumy elementów w tablicy
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+
+        // Obliczanie średniej
+        double average = (double) sum / array.size();
+
+        System.out.println("Suma: " + sum);
+        System.out.println("Średnia: " + average);
+*/
 
 // Napisz program, który sumuje liczby z zakresu 1-40 ale tylko te które są podzielne przez 3
-
-
-
+/*
+        int sum = 0;
+        for (int i = 1; i <= 40; i++) {
+            if (i % 3 == 0) {
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+/*
 // Zadeklaruj tablicę intów długości 5 i wypełnij ja dowolnymi liczbami. Wyświetl całą tablice w pętli for lub while.
-
-
-
+/*
+        int[] tablica = {3, 6, 9, 2, 7};
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.print(tablica[i] + " ");
+        }
+/*
 // Zadeklaruj tablice stringów dlugosci 4, dodaj do niej 4 wyrazy. W petli przypisz każdy wyraz do wcześniej
 // utworzonej zmiennej typu string. Wyświetl tą zmienną
-
 
 
 // Zadeklaruj dwie tablice intów róznej dlugosci. Przypisz obu na indeksie 0 jakas liczbe.
