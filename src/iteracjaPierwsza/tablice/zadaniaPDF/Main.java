a316fb1f34cf902e87d7ed248e17fbab46a4794f
KubaRajewski <66092469+KubaRajewski@users.noreply.github.com>
2023-07-10 12:47:13 +0200

    commit

--- src/iteracjaPierwsza/tablice/zadaniaPDF/Main.java
+++ src/iteracjaPierwsza/tablice/zadaniaPDF/Main.java
@@ -5,16 +5,24 @@ public class Main {
 
 // Napisz program, który dla danej tablicy intów oblicza sumę elementów tej tablicy
 
+        int[] table = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+        int sum = 0;
+        for (int i : table) {
+            sum = sum + table[i];
+        }
+        System.out.println(sum);
 
 // Napisz program, który dla danej tablicy intów długości 3 wyświetli tablicę 2 razy dłuższą,
 // wypełnioną wartością 0 poza ostatnim elementem, ostatni element tablicy ma mieć wartość odpowiadającą długości tablicy.
 
 //                                      {4, 5, 6} → {0, 0, 0, 0, 0, 6}
 
+        int[] table2 = {4, 5, 6};
+        int last = table2[table2.length - 1];
+        System.out.println(last);
 
 // Napisz program, który dla danej tablicy intów zwraca sume elementów po lewej od zadanego indeksu. Np. dla danych:
 // {1,2,3,4,5} i indeksu = 2 suma wynosi 3 bo 1 + 2 = 3
-
 
 
 // Zadeklaruj tablice stringów dlugosci 4, dodaj do niej 4 wyrazy.
