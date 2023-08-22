80c9a683246f6c300d95ff433b31db3cdab360b8
KubaRajewski <66092469+KubaRajewski@users.noreply.github.com>
2023-08-22 23:20:06 +0200

    commit

--- src/obiektowosc/wlasny/model/Main.java
+++ src/obiektowosc/wlasny/model/Main.java
@@ -3,28 +3,4 @@ package obiektowosc.wlasny.model;
 public class Main {
     // PD TODO:
     // stworz swoj biznesowy diagram klas - uml + kod, wszystkie rodzaje atrybutow, dziedizczenie oraz asocajcje
-
-    /*
-     *  Klient kupuje produkty. Klient ma imie nazwisko i rozmiar penisa. Pordukt ma nazwe, cene i jesli jest to kondom to wymiar.
-     *  Klient moze miec wiele produktow, produkt moze miec jednego klienta.
-     *
-     *   1) Napisz mrtode ktota znajduje klienta ktory wydal najwiecej.
-     *
-     *   2) napisz metode ktora zwroci liste klientow ktorzy kupili kondoma
-     *
-     *   3) napisz metode ktora zwroci liste klientow ktorzy kupili kondoma ale nie na swoj rozmiar :D
-     */
-
-        /*
-		 * Stwórz klasę Pracownik (imie,nazwisko, adres, pensja i Klient (imie,nazwisko, ulubiony produkt). Klient kupuje produkty (nazwa, cena).
-			Pracownik uzywa samochod (model, marka)
-
-			1) Znajdz klienta który wydal najwiecej
-
-			2) Wyswietl dla kazdego klienta jego ulubiony produkt (of cuz ulubiony produkt to ma byc atrybut wyliczalny na podstawie zakupow)
-
-			3) Wyswietl 3 top pracownikow z najwieksza pensja (compartor + sortowanie) DODATKOWE
-
-			4) Pracownicy auta moga uzywac wielkorotnie, chcemy przechowywac informacje (i moc ja wyswietlic) ile jaki pracownik uzywal jakiego auta
-		 */
 }
---
+++ src/obiektowosc/zadania/pierwsze/Main.java
@@ -0,0 +1,17 @@
+package obiektowosc.zadania.pierwsze;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        TODO 1
+//        Klient kupuje produkty. Klient ma imie nazwisko i rozmiar penisa. Pordukt ma nazwe, cene i jesli jest to kondom to wymiar
+//        Klient moze miec wiele produktow, produkt moze miec jednego klienta.
+//
+//        1) Napisz mrtode ktota znajduje klienta ktory wydal najwiecej.
+//
+//        2) napisz metode ktora zwroci liste klientow ktorzy kupili kondoma
+//
+//        3) napisz metode ktora zwroci liste klientow ktorzy kupili kondoma ale nie na swoj rozmiar :D
+
+    }
+}
